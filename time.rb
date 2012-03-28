f62da475d70d3841a8f7b1bd491a3389a0f48a2f
e115763 <e115763@gmail.com>
2012-03-28 22:07:02 +0900

    add get_diff , but not tested

--- time.rb
+++ time.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 require 'date'
+require 'pp'
 
 def is_enable? date
   # date が Time かつ 今より後の日付かどうか確認
@@ -12,12 +13,20 @@ end
 
 def get_time str
   # 最初の文字は抜かして12文字取得
+  # YYYYmmddHHMM のフォーマット
   str = str[1,12]
   # string から Time へ
   begin
-    Date.strptime(str,"%Y%m%d%H%M").to_time
+    DateTime.strptime(str + "JST","%Y%m%d%H%M%Z").to_time
   rescue 
     nil
   end
 end
 
+def get_diff time
+  diff = (time - Time.now).to_i
+  day  = (diff / (60 * 60)) / 24
+  hour = (diff / (60 * 60)) % 24
+ 
+  return { "day"=>day , "hour"=>hour }
+end
