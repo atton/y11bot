6acbee4a17ecbcb0687e569d521f546a6b462723
e115763 <e115763@gmail.com>
2012-03-30 10:40:22 +0900

    add get_fotoer_head

--- time.rb
+++ time.rb
@@ -24,9 +24,34 @@ def get_time str
 end
 
 def get_diff time
+  # 時刻差分を取得。
   diff = (time - Time.now).to_i
   day  = (diff / (60 * 60)) / 24
   hour = (diff / (60 * 60)) % 24
- 
+
   return { "day"=>day , "hour"=>hour }
+end
+
+def get_footer_head str
+  # 先頭の数字からフッタの先頭を取得する
+  str = str[0,1]
+
+  # 0 = 無し
+  # 1 = 期限
+  # 2 = 試験
+  # 3 = 補講
+  # default nil
+
+  case str
+  when "0"
+    ""
+  when "1" 
+    "期限"
+  when "2"
+    "試験"
+  when "3"
+    "補講"
+  else 
+    nil 
+  end
 end
