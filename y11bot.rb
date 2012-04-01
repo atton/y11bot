9f04e1ae6c1da8f291caa86eeeaff9d649e879db
e115763 <e115763@gmail.com>
2012-04-01 10:42:48 +0900

    add use twitter code in stub

--- y11bot.rb
+++ y11bot.rb
@@ -4,19 +4,6 @@ require 'date'
 require 'pp'
 require 'Twitter'
 
-# main
-
-Twitter.configure do |config|
-  config.consumer_key = gets.chomp
-  config.consumer_secret = gets.chomp
-  config.oauth_token = gets.chomp
-  config.oauth_token_secret = gets.chomp
-end
-
-Twitter.DirectMessages.each do |dm|
-  pp dm
-end
-
 
 # methods
 
@@ -111,4 +98,44 @@ end
 def get_body str
   # str の2行目以降を返す
   str.sub /^.*\n/,""
+end
+
+def tweet dm,debug
+  # tweet する
+  text = get_body(dm.text) + get_footer(dm.text)
+  puts text
+end
+
+def delete_dm dm,debug
+  # DM を削除する
+  
+  if debug
+    puts "このDMを削除します"
+    puts "id : #{dm.id}"
+    puts "text : #{dm.text}"
+    puts
+    else
+      Twitter.direct_message_destroy dm.id
+    end
+end
+
+# main
+# TODO:fileにmethosを分割する。cronを使うので絶対パスの必要アリ
+
+DEBUG_FLG = true
+
+Twitter.configure do |config|
+  config.consumer_key = gets.chomp
+  config.consumer_secret = gets.chomp
+  config.oauth_token = gets.chomp
+  config.oauth_token_secret = gets.chomp
+end
+
+Twitter.direct_messages.each do |dm|
+  # DMが有効ならばtweet,無効ならばDMを削除する
+  if is_enable? dm.text
+    tweet dm,DEBUG_FLG
+  else
+    delete_dm dm,DEBUG_FLG
+  end
 end
